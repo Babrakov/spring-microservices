f7e24cccce4c9ae94391662b03ba3bbab31dafd7
Sergei Babrakov <s.a.babrakov@gmail.com>
2026-01-19 12:18:39 +0100

    JPA for currency exchange service

---
+++ currency-exchange-service/src/main/java/win/babrakov/currency_exchange_service/CurrencyExchangeController.java
@@ -0,0 +1,30 @@
+package win.babrakov.currency_exchange_service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+@RestController
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+    private final CurrencyExchangeRepository repository;
+
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+
+}
