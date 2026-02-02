3d89bd854950e0ccc686d5b9861bd185e71bd44a
Sergei Babrakov <s.a.babrakov@gmail.com>
2026-02-02 08:56:43 +0100

    Circuit breaker

---
+++ currency-exchange-service/src/main/java/win/babrakov/currency_exchange_service/CircuitBreakerController.java
@@ -0,0 +1,28 @@
+package win.babrakov.currency_exchange_service;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcoded")
+    public String sampleApi(){
+        logger.info("Sample api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcoded( Exception ex){
+        return "fallback response";
+    }
+
+}
