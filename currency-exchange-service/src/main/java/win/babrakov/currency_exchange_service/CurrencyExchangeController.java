a30d38cee0dd1925b1b7d7e399d3ed8de91a86b7
Sergei Babrakov <s.a.babrakov@gmail.com>
2026-02-13 08:36:46 +0100

    Connecting currency exchange to Zipkin

--- currency-exchange-service/src/main/java/win/babrakov/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/win/babrakov/currency_exchange_service/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package win.babrakov.currency_exchange_service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +11,17 @@ import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class CurrencyExchangeController {
 
     private final Environment environment;
     private final CurrencyExchangeRepository repository;
 
-
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        log.info("retrieveExchangeValue called with {} and {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
